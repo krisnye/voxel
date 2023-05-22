b9e44376a698bd92dc8b4e66883880ea0e8e07dc
Kris Nye <krisnye@gmail.com>
2023-05-21 21:15:02 -0700

    added temperature to sample

--- typescript/src/physics/algorithms/test/testAlgorithms.ts
+++ typescript/src/physics/algorithms/test/testAlgorithms.ts
@@ -1,7 +1,7 @@
 import { Volume } from "../../Volume.js";
 import { fillWithTestMaterial } from "../fillWithTestMaterial.js";
 
-const volume = Volume.create([4, 1, 4], { material: Uint8Array });
+const volume = Volume.create([4, 1, 4], { material: Uint8Array, temperature: Float32Array });
 fillWithTestMaterial(volume);
 
-console.log(volume.data.material);
+console.log(volume.data.material, volume.data.temperature);
