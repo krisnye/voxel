833145b28264fb26af10171223d96ca1a79fc30f
Kris Nye <krisnye@gmail.com>
2023-05-30 01:05:55 -0700

    added webgpu types

--- typescript/src/physics/algorithms/test/heatAlgorithms/webGPU.ts
+++ typescript/src/physics/algorithms/test/heatAlgorithms/webGPU.ts
@@ -4,22 +4,59 @@ import { HeatTransferVolumeType } from "../algorithms.test.js";
 export async function webGPU(v: HeatTransferVolumeType, materials: VoxelMaterial[], timeStep: number) {
     // init here.
     if (!globalThis.navigator) {
-        console.log(`NOT IN BROWSER`);
         throw new Error(`Not in browser`);
     }
     if (!navigator.gpu) {
-        console.error(`NO GPU FOR YOU`);
         throw Error("WebGPU not supported.");
     }
 
     const adapter = await navigator.gpu.requestAdapter();
     if (!adapter) {
-        console.error(`NO ADAPTER FOR YOU`);
         throw Error("Couldn't request WebGPU adapter.");
     }
 
     const device = await adapter.requestDevice();
-    console.log(`WEBGPU DEVICE`, device);
+
+    const BUFFER_SIZE = 1000;
+
+    const shader = `
+@group(0) @binding(0)
+var<storage, read_write> output: array<f32>;
+
+@compute @workgroup_size(64)
+fn main(
+  @builtin(global_invocation_id)
+  global_id : vec3u,
+
+  @builtin(local_invocation_id)
+  local_id : vec3u,
+) {
+  // Avoid accessing the buffer out of bounds
+  if (global_id.x >= ${BUFFER_SIZE}) {
+    return;
+  }
+
+  output[global_id.x] =
+    f32(global_id.x) * 1000. + f32(local_id.x);
+}
+`
+
+    const shaderModule = device.createShaderModule({
+        code: shader,
+    });
+
+    const output = device.createBuffer({
+        size: BUFFER_SIZE,
+        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC,
+    });
+
+    const stagingBuffer = device.createBuffer({
+        size: BUFFER_SIZE,
+        usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
+    });
+
+    console.log({ device, shaderModule, output, stagingBuffer });
+
     return async () => {
         console.log("HELLO WEBGPU");
     }
--- typescript/src/website/hooks/useHashState.ts
+++ typescript/src/website/hooks/useHashState.ts
@@ -1,24 +1,24 @@
 import { useEffect, useState } from "preact/hooks"
 
 function getHash() {
-    return location.hash ? location.hash.slice( 1 ) : undefined
+    return location.hash ? location.hash.slice(1) : undefined
 }
 
-export default function useHashState( initialValue?: string ) {
+export default function useHashState(initialValue?: string) {
     const currentHash = getHash()
-    const [ state, setState ] = useState( currentHash ?? initialValue )
-    if ( !currentHash && initialValue )
+    const [state, setState] = useState(currentHash ?? initialValue)
+    if (!currentHash && initialValue)
         location.hash = initialValue
 
-    useEffect( () => {
-        function onHashChange() { setState( getHash() ) }
-        window.addEventListener( "hashchange", onHashChange )
-        return () => window.removeEventListener( "hashchange", onHashChange )
-    }, [] )
+    useEffect(() => {
+        function onHashChange() { setState(getHash()) }
+        window.addEventListener("hashchange", onHashChange)
+        return () => window.removeEventListener("hashchange", onHashChange)
+    }, [])
 
-    function _setState( value: string ) {
+    function _setState(value: string) {
         location.hash = value
     }
 
-    return [ state, _setState ] as [ string, typeof _setState ]
+    return [state, _setState] as [string, typeof _setState]
 }
