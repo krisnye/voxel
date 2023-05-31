3b1d3d78053b9e5c1b6ef799c5e5756e0ecfa077
Kris Nye <krisnye@gmail.com>
2023-05-30 23:18:22 -0700

    first compute shader working

--- typescript/src/physics/algorithms/test/heatAlgorithms/webGPU.ts
+++ typescript/src/physics/algorithms/test/heatAlgorithms/webGPU.ts
@@ -32,9 +32,9 @@ fn main(
   local_id : vec3u,
 ) {
   // Avoid accessing the buffer out of bounds
-  if (global_id.x >= ${BUFFER_SIZE}) {
-    return;
-  }
+//   if (global_id.x >= ${BUFFER_SIZE}) {
+//     return;
+//   }
 
   output[global_id.x] =
     f32(global_id.x) * 1000. + f32(local_id.x);
@@ -55,7 +55,72 @@ fn main(
         usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
     });
 
-    console.log({ device, shaderModule, output, stagingBuffer });
+    const bindGroupLayout = device.createBindGroupLayout({
+        entries: [
+            {
+                binding: 0,
+                visibility: GPUShaderStage.COMPUTE,
+                buffer: {
+                    type: "storage",
+                },
+            },
+        ],
+    });
+
+    const bindGroup = device.createBindGroup({
+        layout: bindGroupLayout,
+        entries: [
+            {
+                binding: 0,
+                resource: {
+                    buffer: output,
+                },
+            },
+        ],
+    });
+
+    const computePipeline = device.createComputePipeline({
+        layout: device.createPipelineLayout({
+            bindGroupLayouts: [bindGroupLayout],
+        }),
+        compute: {
+            module: shaderModule,
+            entryPoint: "main",
+        },
+    });
+
+    const commandEncoder = device.createCommandEncoder();
+    const passEncoder = commandEncoder.beginComputePass();
+    passEncoder.setPipeline(computePipeline);
+    passEncoder.setBindGroup(0, bindGroup);
+    passEncoder.dispatchWorkgroups(Math.ceil(BUFFER_SIZE / 64));
+    passEncoder.end();
+
+    // Copy output buffer to staging buffer
+    commandEncoder.copyBufferToBuffer(
+        output,
+        0, // Source offset
+        stagingBuffer,
+        0, // Destination offset
+        BUFFER_SIZE
+    );
+
+    // End frame by passing array of command buffers to command queue for execution
+    device.queue.submit([commandEncoder.finish()]);
+
+    // map staging buffer to read results back to JS
+    await stagingBuffer.mapAsync(
+        GPUMapMode.READ,
+        0, // Offset
+        BUFFER_SIZE // Length
+    );
+
+    const copyArrayBuffer = stagingBuffer.getMappedRange(0, BUFFER_SIZE);
+    const data = copyArrayBuffer.slice();
+    stagingBuffer.unmap();
+    console.log(new Float32Array(data));
+
+    console.log({ device, shaderModule, output, stagingBuffer, bindGroupLayout, bindGroup, computePipeline, commandEncoder, passEncoder });
 
     return async () => {
         console.log("HELLO WEBGPU");
